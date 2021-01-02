652572f9797e3e18e9948ae789dc7f4541c374cf
Junaid Sikander <Junaid_sikander@live.com>
2021-01-02 22:24:45 +0500

    [ADD] connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import * as socketIO from 'socket.io';
+import http from 'http';
 import {dirname, join} from 'path';
 import {fileURLToPath} from 'url';
 
@@ -8,9 +10,22 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const server = http.createServer(app);
+const io = new socketIO.Server(server);
+
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const clientPath = join(__dirname, '../client');
 
 app.use(express.static(clientPath));
 
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`));
+io.on('connection', (socket) => {
+    console.log('new user Connected');
+
+    socket.on('disconnect', (socket) => {
+        console.log('Disconnect from client');
+    });
+});
+
+
+server.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`));
