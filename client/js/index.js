fa474b6376255f95ef5b4cc75d09d908058ef6e8
Junaid Sikander <Junaid_sikander@live.com>
2021-01-04 11:29:44 +0500

    [ADD] scroll to bottom if user receive new message

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,23 @@
 let socket = io();
 
+
+//Scrolling to botton func
+function scrollToBottom() {
+    // Selectors
+    let messages = jQuery('#messages');
+    let newMessage = messages.children('li:last-child');
+    // Heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -13,6 +31,7 @@ socket.on('newMessage', function (msg) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('disconnect', function () {
